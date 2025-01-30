4fb3c2d1576a24a51d676000115264c76c4de77e
Fedee <federicovernet@gmail.com>
2025-01-30 13:27:14 -0300

    componentes onliner

--- parte1_React/src/App.jsx
+++ parte1_React/src/App.jsx
@@ -1,3 +1,19 @@
+
+/*
+cuando el component es tan corto se puede usar el oneLiner
+
+const Header = ({course}) => <h1>{course}</h1>
+*/
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+const Content = (props) => {
+  return <p>{props.part} {props.ejercicios}</p>
+}
+const Total = (props) => {
+  return <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +25,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}>  </Header>
+      <Content part={part1} ejercicios={exercises1}></Content>
+      <Content part={part2} ejercicios={exercises2}></Content>
+      <Content part={part3} ejercicios={exercises3}></Content>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3}></Total>
     </div>
   )
 }
