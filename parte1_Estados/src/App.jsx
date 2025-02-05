c68053e8dd8cf7f3c2042f2260b570c3723dd6f4
Fedee <federicovernet@gmail.com>
2025-02-04 21:13:49 -0300

    ejercicios1.6 - estados,componentes,tablas

--- parte1_Estados/src/App.jsx
+++ parte1_Estados/src/App.jsx
@@ -1,38 +1,131 @@
 import { useState } from 'react'
-import "./css/Button.css"
 
-/** Separar distintas funciones en simples componentes es una muy buena practica */
-const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ clickValue, text }) => {
+  return <button onClick={clickValue}>{text}</button>
+}
 
-/** Creamos tambien un componente de button */
+//UN COMPONENTE BASICAMENTE RETORNA JSX HTML, CSS
+//Su objetivo principal es renderizar una parte de la interfaz de usuario. COMO EL BTN DE ARRIBA
 
-/* Por convención, las props de los controladores de eventos deberían iniciar con on, 
-seguido de una letra mayúscula. 
-Por ejemplo, el prop onClick del componente Button pudo haberse llamado onSmash: */
-const Button = ({ onSmash, text }) => <button className="button2" onClick={onSmash}>{text}</button>
+
+const Statistics = ({ clicks, prom, positive }) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={clicks.good} />
+          <StatisticsLine text="neutral" value={clicks.neutral} />
+          <StatisticsLine text="bad" value={clicks.bad} />
+          <StatisticsLine text="total" value={clicks.total} />
+          <StatisticsLine text="Promedio" value={prom} />
+          <StatisticsLine text="Positivo" value={positive + "%"} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const History = ({ clicks, prom, positive }) => {
+  if (clicks.total === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Statistics clicks={clicks} prom={prom} positive={positive}></Statistics>
+    </div >
+  )
+}
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  // guarda los clics de cada botón en su propio estado
+  /*  Version con 3 estados
 
-  /** lo ideal es separar estas funciones en 2 y reutilizarlas */
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
-  const decreaseByOne = () => setCounter(counter - 1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  /*
+    const clickGood = () => {
+      setGood(good + 1)
+    }
+    const clickNeutral = () => {
+      setNeutral(neutral + 1)
+    }
+    const clickBad = () => {
+      setBad(bad + 1)
+    }
+*/
+  //Lo podemos hacer con 1 solo estado
+  const [clicks, setClicks] = useState({
+    good: 0, neutral: 0, bad: 0, total: 0
+  })
 
-  /**El propio tutorial oficial de React sugiere: "En React, 
-   * es convencional usar nombres onSomething para props que representan eventos
-   *  y handleSomething para las definiciones de funciones que controlan los eventos." */
+  /* que los eventos se encarguen de cambiar el estado
+  const clickGood = () => {
+    const newClicks = {
+      // En la práctica, { ...clicks } crea un nuevo objeto que tiene copias de todas las propiedades del objeto clicks
+      ...clicks,
+      good: clicks.good + 1,
+    }
+    setClicks(newClicks)
+  }*/
+  //SON EQUIVALENTES
 
-  /** con onClick contralamos Eventos */
+  const clickGood = () => {
+    setClicks({ ...clicks, good: clicks.good + 1, total: clicks.total + 1 })
+  }
+
+  const clickNeutral = () => {
+    const newClicks = {
+      ...clicks,
+      neutral: clicks.neutral + 1,
+      total: clicks.total + 1,
+    }
+    setClicks(newClicks)
+  }
+  const clickBad = () => {
+    const newClicks = {
+      ...clicks,
+      bad: clicks.bad + 1,
+      total: clicks.total + 1,
+    }
+    setClicks(newClicks)
+  }
+
+
+  const calculatePromedio = () => {
+    const promedio = clicks.good * 1 + clicks.neutral * 0 + clicks.bad * -1;
+    return clicks.total > 0 ? (promedio / clicks.total).toFixed(9) : 0;
+  }
+
+  const calculatePositivePercentage = () => {
+    return clicks.total > 0 ? ((clicks.good / clicks.total) * 100).toFixed(9) : 0;
+  };
+
+
   return (
     <div>
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne} text="plus" />
-      <Button onSmash={setToZero} text="zero" />
-      <Button onSmash={decreaseByOne} text="minus" />
+      <h1>give feedback</h1>
+      <Button clickValue={clickGood} text="good" />
+      <Button clickValue={clickNeutral} text="neutral" />
+      <Button clickValue={clickBad} text="bad" />
+      <History clicks={clicks} prom={calculatePromedio()} positive={calculatePositivePercentage()} />
     </div>
-
   )
 }
 
---
+++ parte1_Estados/src/App2.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import "./css/Button.css"
+
+/** Separar distintas funciones en simples componentes es una muy buena practica */
+const Display = ({ counter }) => <div>{counter}</div>
+
+/** Creamos tambien un componente de button */
+
+/* Por convención, las props de los controladores de eventos deberían iniciar con on, 
+seguido de una letra mayúscula. 
+Por ejemplo, el prop onClick del componente Button pudo haberse llamado onSmash: */
+const Button = ({ onSmash, text }) => <button className="button2" onClick={onSmash}>{text}</button>
+
+
+const App2 = () => {
+    const [counter, setCounter] = useState(0)
+
+    /** lo ideal es separar estas funciones en 2 y reutilizarlas 
+     * Controladores de Eventos.
+    */
+    const increaseByOne = () => setCounter(counter + 1)
+    const setToZero = () => setCounter(0)
+    const decreaseByOne = () => setCounter(counter - 1)
+
+    /**El propio tutorial oficial de React sugiere: "En React, 
+     * es convencional usar nombres onSomething para props que representan eventos
+     *  y handleSomething para las definiciones de funciones que controlan los eventos." */
+
+    /** con onClick contralamos Eventos */
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button onSmash={increaseByOne} text="plus" />
+            <Button onSmash={setToZero} text="zero" />
+            <Button onSmash={decreaseByOne} text="minus" />
+        </div>
+
+    )
+}
+
+export default App2
